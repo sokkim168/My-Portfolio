6e09ecfa0391f769d5b56ff4b4adc3ede3438030
sokkim roth <34774632+kimsok@users.noreply.github.com>
2024-07-18 16:46:13 +0700

    update icon

--- src/components/Master.vue
+++ src/components/Master.vue
@@ -5,10 +5,10 @@
 
 <template>
   <div class="flex flex-col h-screen">
-    <header>&nbsp;</header>
+
     <main class="flex-auto">
       <div class="relative">
-        <ComponentHeader></ComponentHeader>
+        <header><ComponentHeader></ComponentHeader></header>
         <slot></slot>
       </div>
     </main>
